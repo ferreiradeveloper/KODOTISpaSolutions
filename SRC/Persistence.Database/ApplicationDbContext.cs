0fc820b0076cfda2a4c40d6218ba9f85ff9d954e
Francisco Ferreira de la cruz <ferreiradelacruz@hotmail.com>
2020-01-14 00:09:10 +0100

    Modelos Creados y DbContext, Faltarian las migraciones parea finalizar el modulo

---
+++ SRC/Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
